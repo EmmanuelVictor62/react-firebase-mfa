809e2cbfd61846a8afc9c8054faee4b8f4bb2e28
royalManuel <104584134+EmmanuelVictor62@users.noreply.github.com>
2025-01-20 01:30:40 +0100

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Firebase MFA",
+  description: "Test project for Firebase MFA",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable}`}>{children}</body>
+    </html>
+  );
+}
